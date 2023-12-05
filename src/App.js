5943b069826db689b762a59d2dca3d2a6168beca
Anton Mamontov <a.mamontov@p-s.kz>
2023-12-05 19:05:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const button = document.querySelector('button')
+
+  button.addEventListener('click', () => {
+    Notification.requestPermission().then(perm => {
+      if(perm === 'granted') {
+        new Notification("Fuck OGG")
+      }
+    })
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button>clik</button>
       </header>
     </div>
   );
